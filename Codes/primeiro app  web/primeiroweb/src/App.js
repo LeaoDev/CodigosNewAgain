b4642c61209212d5831c3d92e716e152ff2a2012
Marcelo Leao <leao.marcelo1@hotmail.com>
2019-09-25 19:22:43 -0300

    fim do curso react

--- Codes/primeiro app  web/primeiroweb/src/App.js
+++ Codes/primeiro app  web/primeiroweb/src/App.js
@@ -1,16 +1,17 @@
 import React, {Component} from "react";
 
+import Routes from "./routes";
 
 import './styles.css';
 
 import Header from "./components/Header";
-import Main from './pages/main';
+import Main from "./pages/main";
 
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
 
 
 
--- Codes/primeiro app  web/primeiroweb/src/pages/main/index.js
+++ Codes/primeiro app  web/primeiroweb/src/pages/main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 import "./styles.css";
-
+import {Link} from 'react-router-dom';
 
 
 
@@ -65,7 +65,7 @@ export default class Main extends Component{
                       <strong>{product.title}</strong>
                       <p>{product.description}</p>
 
-                      <a href="">Acessar</a>
+                      <Link to={`/products/${product._id}`}>Acessar</Link>
                   </article>
               ))}  
               <div className="actions">
